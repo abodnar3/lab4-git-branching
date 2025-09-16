621c4500f6c90a6f54fe3bdffb40c06967a72c91
Andrew Bodnar <abodnar3@users.noreply.github.com>
2025-09-16 14:12:36 -0400

    Added a center path.

--- story.py
+++ story.py
@@ -1,10 +1,12 @@
 def intro():
-    print("You wake up in a dark forest. You can go left or right.")
-    choice = input("Which direction do you choose? (left/right): ").strip().lower()
+    print("You wake up in a dark forest. You can go left, forward, or right.")
+    choice = input("Which direction do you choose? (left/center/right): ").strip().lower()
     if choice == "left":
         left_path()
     elif choice == "right":
         right_path()
+    elif choice == "center":
+        center_path()
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
 
@@ -13,6 +15,14 @@ def left_path():
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+
+def center_path():
+    print("You walk forward and find a abnormally large tree with a door on its trunk.")
+    print("You open the door, and enter the tree to find only a warm, cozy bed inside.")
+    print("You are tired and confused. You decide to close the door and sleep in the bed.")
+    print("You wake up again. This time, however, you find yourself in your childhood bedroom.")
+    print("It was all a dream.")
+    print("Right?")
 
 if __name__ == "__main__":
     intro()
